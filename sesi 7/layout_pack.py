1630e2a732afb19c058bd1dc560c9d93c7557189
Mifta Farid <mifta.fh@gmail.com>
2026-02-01 19:57:28 +0700

    Update layout_pack.py

--- sesi 7/layout_pack.py
+++ sesi 7/layout_pack.py
@@ -1,16 +1,18 @@
-import tkinter as tk
+import tkinter as tk # import library tkinter
 
 window = tk.Tk()
-window.title("app GUI")
-window.geometry("300x200")
+window.title("App GUI")
 
-#Label 1
+# Label 1
 label_1 = tk.Label(window, text="Label 1")
 label_1.pack()
-#buton 1
-button = tk.Button(window, text="Button)
-button.pack()
-#label 2
+
+# button 1
+button_1 = tk.Button(window, text="Button")
+button_1.pack()
+
+# Label 2
 label_2 = tk.Label(window, text="Label 2")
 label_2.pack()
+
 window.mainloop()
