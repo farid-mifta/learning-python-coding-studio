32fa15263c114d825cffd121f06d216c915852b4
Mifta Farid <mifta.fh@gmail.com>
2026-02-01 19:55:21 +0700

    Update Layout_grid.py

--- sesi 7/Layout_grid.py
+++ sesi 7/Layout_grid.py
@@ -1,12 +1,15 @@
 import tkinter as tk
 
 window = tk.Tk()
-window.title ("app Gui")
-window.geometry("300x200")
+window.title("Layout Grid")
 
-#label
-label_nama = tk.Label(window, text="Nama: ")
-label_nama.grid(row=0, column = 0)
+# Label dan Entry dalam grid
+tk.Label(window, text="Nama:").grid(row=0, column=0, padx=5, pady=5)
+tk.Entry(window).grid(row=0, column=1, padx=5, pady=5)
 
-entry_nama = tk.entry(window)
-entry_nama.grid(row = 0, colomn = ` `)
+tk.Label(window, text="Umur:").grid(row=1, column=0, padx=5, pady=5)
+tk.Entry(window).grid(row=1, column=1, padx=5, pady=5)
+
+tk.Button(window, text="Simpan").grid(row=2, column=0, columnspan=2, pady=10)
+
+window.mainloop()
